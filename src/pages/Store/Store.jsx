e351b5ba796867b392f44d61c7ca9cb13ad5cc81
Ajarek <ajarek@poczta.onet.pl>
2023-05-11 23:10:30 +0200

    added data.json

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 import './Store.css'
 const Store = () => {
   return (
-    <div>Store</div>
+    <div className='store'>
+      <h1>
+        <span className='black'>Nasz </span>
+        <span className='pink'>Sklep</span>
+      </h1>
+      <div className='products'>
+        <button>Wszystko</button>
+        <button>Ciastka</button>
+        <button>Babeczki</button>
+        <button>Słodycze</button>
+        <button>Pączki</button>
+      </div>
+      <div className="search"><input type="search" name="" id="" /></div>
+      <div className="list">
+        <div className="card">
+          <div className="card-img">
+            <img src="/img/ciasto1.jpg" alt="ciastko" />
+          </div>
+          <div className="product-data">
+              <span className='product-name'>ciasto1 </span>    
+              <span className='product-price'>12 PLN</span>    
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
