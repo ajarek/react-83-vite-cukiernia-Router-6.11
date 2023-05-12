679f35f1938bb2cd1458b92cd2ab18e7283f330e
Ajarek <ajarek@poczta.onet.pl>
2023-05-12 22:36:11 +0200

    added search

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -1,5 +1,5 @@
-import{useState} from 'react'
-import { useLoaderData} from 'react-router-dom'
+import { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 import data from '../../assets/data.json'
 import './Store.css'
 export const storeLoader = () => {
@@ -7,9 +7,10 @@ export const storeLoader = () => {
   return products
 }
 const Store = () => {
-  const [filterProducts,setFilterProducts]=useState('all')
+  const [filterProducts, setFilterProducts] = useState('all')
+  const [searchText, setSearchText] = useState('')
   const products = useLoaderData()
- console.log(filterProducts)
+  console.log(filterProducts)
   return (
     <div className='store'>
       <h1>
@@ -17,30 +18,56 @@ const Store = () => {
         <span className='pink'>Sklep</span>
       </h1>
       <div className='products'>
-        <button onClick={()=>setFilterProducts('all')}>Wszystko</button>
-        <button onClick={()=>setFilterProducts('ciastka')}>Ciastka</button>
-        <button onClick={()=>setFilterProducts('babeczki')}>Babeczki</button>
-        <button onClick={()=>setFilterProducts('słodycze')}>Słodycze</button>
-        <button onClick={()=>setFilterProducts('pączki')}>Pączki</button>
+        <button onClick={() => setFilterProducts('all')}>Wszystko</button>
+        <button onClick={() => setFilterProducts('ciastka')}>Ciastka</button>
+        <button onClick={() => setFilterProducts('babeczki')}>Babeczki</button>
+        <button onClick={() => setFilterProducts('słodycze')}>Słodycze</button>
+        <button onClick={() => setFilterProducts('pączki')}>Pączki</button>
       </div>
-      <div className="search"><input type="search" placeholder='🔍 Szukaj' id="" /></div>
-      <div className="list">
-        {products 
-        .filter(pr=>pr.category===filterProducts||filterProducts==='all')
-        .map((dt)=>{
-          return(
-        <div className="card" key={dt.id}>
-          <div className="card-img">
-            <img src={dt.src} alt="ciastko" />
-          </div>
-          <div className="product-data">
-              <span className='product-name'>{dt.name} </span>    
-              <span className='product-price'>{dt.price} PLN</span>
-              <span id={dt.id} className='add-cart' onClick={(e)=>console.log(e.target.id)}>🛒</span>    
-          </div>
-        </div>
-        )
-       })}
+
+      <div className='search'>
+        <input
+          type='search'
+          placeholder='🔍 Szukaj'
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+
+      <div className='list'>
+        {products
+          .filter((pr) =>
+            pr.name.toLowerCase().includes(searchText.toLowerCase())
+          )
+          .filter(
+            (pr) => pr.category === filterProducts || filterProducts === 'all'
+          )
+          .map((dt) => {
+            return (
+              <div
+                className='card'
+                key={dt.id}
+              >
+                <div className='card-img'>
+                  <img
+                    src={dt.src}
+                    alt='ciastko'
+                  />
+                </div>
+                <div className='product-data'>
+                  <span className='product-name'>{dt.name} </span>
+                  <span className='product-price'>{dt.price} PLN</span>
+                  <span
+                    id={dt.id}
+                    className='add-cart'
+                    onClick={(e) => console.log(e.target.id)}
+                  >
+                    🛒
+                  </span>
+                </div>
+              </div>
+            )
+          })}
       </div>
     </div>
   )
